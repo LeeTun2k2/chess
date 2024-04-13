62d399141939921985239721a38e4e3f1487515e
Le Quang Tung <lequangtung2002@gmail.com>
2024-04-14 00:02:14 +0700

    feat: blog detail

--- front-end/src/pages/club/blog.jsx
+++ front-end/src/pages/club/blog.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Container,
   Box,
@@ -7,25 +7,82 @@ import {
   Heading,
   VStack,
   Text,
+  useToast,
+  Image,
+  Divider,
 } from "@chakra-ui/react";
 import { useCurrentPath } from "../../lib/hooks/route";
 import ClientLayout from "../../components/layouts/clientLayout";
 import { IoDocumentText } from "react-icons/io5";
 import DocNav from "../../components/nav/doc_nav";
+import { useTranslation } from "react-i18next";
+import axios from "../../lib/axios";
+import { toast_error } from "../../lib/hooks/toast";
+import appSettings from "../../settings/appSettings";
+import { formatDate } from "../../lib/datetime";
+import ReactHtmlParser from "html-react-parser";
 
 export default function BlogPage(props) {
   const path = useCurrentPath();
-  const slug = path[path.length - 1];
+  const id = path[path.length - 1];
+  const toast = useToast();
+  const theme = localStorage.getItem("theme");
+  const { t } = useTranslation();
+  const [data, setData] = useState({});
 
-  const getBlog = () => {
-    return {};
-  };
-
-  const data = getBlog();
+  useEffect(() => {
+    axios
+      .get(`${appSettings.API_PROXY}/blogs/${id}`)
+      .then((resp) => {
+        setData(resp?.data?.blog ?? {});
+      })
+      .catch((err) => {
+        if (err?.response) toast(toast_error(err?.response?.data));
+        else toast(toast_error("Something went wrong. Please try again."));
+      });
+  }, [toast]);
 
   return (
     <ClientLayout>
-      <Container maxW="6xl" py={8}></Container>
+      <Container maxW="6xl" py={8}>
+        <Flex>
+          <Box w={"66%"}>
+            <Flex align={"center"}>
+              <Text w={"20%"} fontSize={"2xl"} fontWeight={"bold"}>
+                {t("blogs.blog")} {" > "}
+              </Text>
+              <Heading fontSize={"2xl"} textAlign={"justify"}>
+                {data.title}
+              </Heading>
+            </Flex>
+            <Flex>
+              <Text>{data.description}</Text>
+              <Spacer />
+              <Text
+                fontSize={"sm"}
+                color={"gray"}
+                w={"25%"}
+                textAlign={"right"}
+              >
+                {t("common.created_at")} {formatDate(data.created_at)}
+              </Text>
+            </Flex>
+            <Divider mb={4} borderColor={theme === "dark" ?? "black"} />
+            <Image
+              w={"100%"}
+              src={`${appSettings.API_PROXY}/images/${data.image}`}
+              alt={data.title}
+              objectFit={"cover"}
+              borderRadius={4}
+            />
+            <Box mt={4}>{ReactHtmlParser(data.content ?? "")}</Box>
+          </Box>
+          <Spacer />
+          <Box w={"30%"}>
+            <DocNav />
+          </Box>
+        </Flex>
+      </Container>
     </ClientLayout>
   );
 }
