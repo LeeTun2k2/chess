ad1d4d332888cb937f1fc360a66fde31b4e217a9
Le Quang Tung <lequangtung2002@gmail.com>
2024-04-13 13:03:25 +0700

    up: update chat bot ui

--- front-end/src/components/chat/chatbox.jsx
+++ front-end/src/components/chat/chatbox.jsx
@@ -1,95 +1,195 @@
 import React, { useState, useEffect } from "react";
-import { Box, Text, Input, Button, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Input,
+  Button,
+  Text,
+  HStack,
+  Flex,
+  VStack,
+  Spacer,
+} from "@chakra-ui/react";
+import { getUserData } from "../../lib/auth";
+import { IoSend } from "react-icons/io5";
 import OpenAI from "openai";
-import { OPENAI_KEY } from "../../settings/appSettings"
+import { OPENAI_KEY } from "../../settings/appSettings";
+import { useTranslation } from "react-i18next";
 
-const ChatPage = () => {
-  const [messages, setMessages] = useState([]);
-  const [inputText, setInputText] = useState("");
+const ChatBox = () => {
+  const user = getUserData();
+  const theme = localStorage.getItem("theme");
+  const { t } = useTranslation();
   const [openaiInstance, setOpenAIInstance] = useState(null);
-  const chessSuggestions = ["What's your next move?", "How about playing Nf3?", "Consider castling kingside."];
+  const [messages, setMessages] = useState([
+    {
+      text: t("chat.hello"),
+      username: t("chat.assistant"),
+    },
+  ]);
+  const [inputValue, setInputValue] = useState("");
+
+  const suggestions = [
+    t("chat.about_ute_chess_club"),
+    t("chat.when_club_offline"),
+    t("chat.how_to_play_chess"),
+  ];
 
   useEffect(() => {
-    // Initialize OpenAI instance
-    const openai = new OpenAI({ apiKey: OPENAI_KEY, dangerouslyAllowBrowser: true });
+    const openai = new OpenAI({
+      apiKey: OPENAI_KEY,
+      dangerouslyAllowBrowser: true,
+    });
     setOpenAIInstance(openai);
   }, []);
 
+  const sendChessSuggestion = async (suggestion) => {
+    setInputValue(suggestion);
+    sendMessage();
+  };
+
   const sendMessage = async () => {
-    if (inputText.trim() === "") return;
+    if (inputValue.trim() === "") return;
 
+    const old_messages = messages;
     const message = {
-      role: "user",
-      content: inputText,
+      text: inputValue,
+      user_id: user?.id,
+      username: user?.username,
     };
-
     setMessages([...messages, message]);
-    setInputText("");
+    setInputValue("");
+
+    const gpt_message = {
+      role: "user",
+      content: inputValue,
+    };
 
     try {
       const completion = await openaiInstance.chat.completions.create({
         messages: [
           { role: "system", content: "You are a helpful assistant." },
-          ...messages,
-          message
+          ...old_messages.map((item) => {
+            return { role: item.id ? "user" : "assistant", content: item.text };
+          }),
+          gpt_message,
         ],
         model: "gpt-3.5-turbo",
-        max_tokens: 50
+        max_tokens: 50,
       });
 
       const botMessage = {
-        role: "assistant",
-        content: completion.choices[0].message.content,
+        username: t("chat.assistant"),
+        text: completion.choices[0].message.content,
       };
 
-      setMessages([...messages, botMessage]);
+      setMessages([...messages, message, botMessage]);
     } catch (error) {
       console.error("Error sending message:", error);
     }
-  };
-
-  const handleInputChange = (event) => {
-    setInputText(event.target.value);
   };
 
   const clearChat = () => {
     setMessages([]);
   };
 
-  const sendChessSuggestion = async (suggestion) => {
-    setInputText(suggestion);
-    sendMessage();
-  };
-
   return (
-    <Box p="4" boxShadow="lg" borderRadius="md">
-      <Box h="300px" overflowY="auto" mb="4">
+    <Box w={"100%"} mx="auto">
+      <Flex
+        p={4}
+        mb={4}
+        borderRadius="md"
+        boxShadow={4}
+        h={400}
+        overflowY="auto"
+        scrollBehavior={"smooth"}
+        border={"1px solid lightgray"}
+        flexDir={"column"}
+        position={"relative"}
+      >
         {messages.map((message, index) => (
-          <Box key={index} alignSelf={message.role === "user" ? "flex-end" : "flex-start"} bg={message.role === "user" ? "blue.100" : "gray.100"} p="2" borderRadius="md" mb="2">
-            <Text>{message.content}</Text>
+          <Box
+            key={index}
+            mb={2}
+            maxW={"80%"}
+            alignSelf={user?.id === message.user_id ? "flex-end" : "flex-start"}
+          >
+            <Text
+              fontSize={"x-small"}
+              color={theme === "dark" ? "white" : "gray.500"}
+              textAlign={user?.id === message.user_id ? "right" : "left"}
+              mx={1}
+            >
+              @{message.username}
+            </Text>
+            <Text
+              bgColor={
+                user?.id === message.user_id
+                  ? theme === "dark"
+                    ? "black"
+                    : "gray.100"
+                  : theme === "dark"
+                    ? "black"
+                    : "lightgray"
+              }
+              textAlign={user?.id === message.user_id ? "right" : "left"}
+              color={theme === "dark" ? "white" : "black"}
+              px={4}
+              py={1}
+              borderRadius={8}
+            >
+              {message.text}
+            </Text>
           </Box>
         ))}
-      </Box>
-      <Flex align="center" justify="space-between" padding={2}>
-        <Input
-          flex="1"
-          value={inputText}
-          onChange={handleInputChange}
-          placeholder="Type a message..."
-          style={{ whiteSpace: "pre-wrap" }}
-        />
-        
+        {messages?.length < 2 && (
+          <Flex
+            position={"absolute"}
+            flexDir={"column"}
+            align={"center"}
+            bottom={0}
+            w={"100%"}
+            px={4}
+            left={0}
+          >
+            {suggestions.map((item, idx) => (
+              <Button
+                variant={"outline"}
+                colorScheme="gray"
+                borderRadius={20}
+                key={idx}
+                w={"100%"}
+                mb={2}
+                onClick={async () => {
+                  await sendMessage();
+                }}
+              >
+                {item}
+              </Button>
+            ))}
+          </Flex>
+        )}
       </Flex>
-      <Button colorScheme="blue" onClick={sendMessage}>Send</Button>
-        <Button colorScheme="red" onClick={clearChat}>Clear Chat</Button>
-      <Box mt="4">
-        <Text fontWeight="bold" mb="2">Chess Suggestions:</Text>
-        {chessSuggestions.map((suggestion, index) => (
-          <Button key={index} onClick={() => sendChessSuggestion(suggestion)} mr="2">{suggestion}</Button>
-        ))}
-      </Box>
+      {user?.id ? (
+        <HStack>
+          <Input
+            placeholder="Type your message here..."
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target?.value)}
+            colorScheme="gray"
+            variant={"outline"}
+            borderColor={"lightgray"}
+          />
+          <Button colorScheme="gray" onClick={sendMessage}>
+            <IoSend fontSize={24} />
+          </Button>
+        </HStack>
+      ) : (
+        <Text color={"gray"} fontSize={"sm"} align={"center"}>
+          {t("chat.please_login")}
+        </Text>
+      )}
     </Box>
   );
 };
 
-export default ChatPage;
+export default ChatBox;
