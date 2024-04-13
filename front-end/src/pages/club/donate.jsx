6b69157a91a8e9f73cdcac925326bc7956ffe20e
Le Quang Tung <lequangtung2002@gmail.com>
2024-04-14 00:14:53 +0700

    up: update donate

--- front-end/src/pages/club/donate.jsx
+++ front-end/src/pages/club/donate.jsx
@@ -1,30 +1,53 @@
-import { Container, Heading, Text, Button, Link } from "@chakra-ui/react";
+import {
+  Container,
+  Heading,
+  Text,
+  Button,
+  Link,
+  Image,
+  useToast,
+} from "@chakra-ui/react";
 import ClientLayout from "../../components/layouts/clientLayout";
+import { useTranslation } from "react-i18next";
+import { useEffect, useState } from "react";
+import axios from "../../lib/axios";
+import { toast_error } from "../../lib/hooks/toast";
+import appSettings from "../../settings/appSettings";
 
 export default function DonatePage() {
+  const { t } = useTranslation();
+  const toast = useToast();
+  const [image, setImage] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`${appSettings.API_PROXY}/donate`)
+      .then((resp) => {
+        setImage(resp?.data?.image ?? {});
+      })
+      .catch((err) => {
+        if (err?.response) toast(toast_error(err?.response?.data));
+        else toast(toast_error("Something went wrong. Please try again."));
+      });
+  }, [toast]);
+
   return (
     <ClientLayout>
       <Container maxW="6xl" py={8}>
         <Heading as="h1" mb={4}>
-          Support SPKT Chess Club
+          {t("club.support_ute_chess_club")}
         </Heading>
 
         <Text fontSize="lg" mb={4}>
-          Your donation helps us continue providing quality chess education and
-          organizing events for our members.
+          {t("club.your_donate_help_us_providing")}
         </Text>
-
-        <Text fontSize="lg" mb={4}>
-          To make a donation, please click the button below:
-        </Text>
-
-        <Button colorScheme="teal" size="lg" mb={4}>
-          <Link href="https://www.paypal.com" isExternal>
-            Donate Now
-          </Link>
-        </Button>
-
-        <Text fontSize="lg">Thank you for your support!</Text>
+        <Image
+          h={200}
+          w={200}
+          src={image}
+          alt={t("club.support_ute_chess_club")}
+        />
+        <Text fontSize="lg">{t("club.thank_you")}</Text>
       </Container>
     </ClientLayout>
   );
