5510bb8579a6b36642431fddef4f115227420403
Le Quang Tung <lequangtung2002@gmail.com>
2024-05-01 02:23:02 +0700

    feat: initial blitz-static

---
+++ blitz-tactics/.docker/postgres_migrate.sh
@@ -0,0 +1,1 @@
+docker exec -it docker-blitz-tactics-1 sh -c "rails db:create && rails db:migrate && yarn lichess:puzzles:fetch && yarn lichess:puzzles:import && yarn lichess:puzzles:check && yarn blitz:game_modes:fetch && yarn blitz:game_modes:import && yarn blitz:game_modes:check"
