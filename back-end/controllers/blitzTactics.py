0b366044da99da8148230f73ee203bd56d549e76
Le Quang Tung <lequangtung2002@gmail.com>
2024-05-02 14:33:45 +0700

    up: update blitztactic url

--- back-end/controllers/blitzTactics.py
+++ back-end/controllers/blitzTactics.py
@@ -3,7 +3,7 @@ from flask_jwt_extended import jwt_required
 
 blitztactic_bp = Blueprint('blitztactics', __name__)
 
-BLITZ_TACTICS_URL = "blitz-tactics:3000"
+BLITZ_TACTICS_URL = "http://chess_blitz-tactics:3000"
 
 @blitztactic_bp.route('/api/blitz-tactics/haste', methods=['GET'])
 def pass_haste():
