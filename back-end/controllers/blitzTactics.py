a76aba59a60b6df53e62f486bcf4a8d0f4fcabf3
Le Quang Tung <lequangtung2002@gmail.com>
2024-05-02 17:03:42 +0700

    up: make game more secue by hash url

--- back-end/controllers/blitzTactics.py
+++ back-end/controllers/blitzTactics.py
@@ -5,7 +5,7 @@ import base64
 
 blitztactic_bp = Blueprint('blitztactics', __name__)
 
-BLITZ_TACTICS_URL = "https://chess.workon.space/blitz-tactics"
+BLITZ_TACTICS_URL = "https://chess.workon.space/UcG7F4xRfTzW1eVtLyPmNkQiOoAhDjBsErXuZvCwYxJbGzHpQqKiUlPfKsJdMgInLbHnVcXdFgBvNcMdHgXjZkLpWmRnPiOuYqAtWsEdRfYgUhIjOlP"
 
 user_service = UserService()
 
