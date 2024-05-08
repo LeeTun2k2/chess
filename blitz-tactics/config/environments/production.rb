679d44c524513cab1e88258f21838213300f5f76
Le Quang Tung <lequangtung2002@gmail.com>
2024-05-08 20:27:34 +0700

    up: update serve nginx static file

--- blitz-tactics/config/environments/production.rb
+++ blitz-tactics/config/environments/production.rb
@@ -2,12 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts = [
-    IPAddr.new("0.0.0.0/0"), # All IPv4 addresses.
-    IPAddr.new("::/0"),      # All IPv6 addresses.
-    "localhost",             # The localhost reserved domain.
-    ENV["SERVER_HOST_NAME"]  # Allow this to be addressed when running in containers via docker-compose.yml.
-  ]
+  config.hosts.clear
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -34,8 +29,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.digest = true
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
