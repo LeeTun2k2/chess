1d91de534a494b8bf9397be31b28dda84168de59
Le Quang Tung <lequangtung2002@gmail.com>
2024-06-01 14:02:13 +0700

    up: fix production endpoint

--- back-end/controllers/others.py
+++ back-end/controllers/others.py
@@ -63,8 +63,8 @@ def create_payment():
     secretKey = "K951B6PE1waDMi640xX08PD3vg6EkVlz"
     orderInfo = "pay with MoMo"
     partnerCode = "MOMO"
-    redirectUrl = "http://localhost:3000/billing"
-    ipnUrl = "http://localhost:3000/billing"
+    redirectUrl = "https://chess.workon.space/billing"
+    ipnUrl = "https://chess.workon.space/billing"
     amount = request.json.get('amount')
     orderId = str(uuid.uuid4())
     requestId = str(uuid.uuid4())
