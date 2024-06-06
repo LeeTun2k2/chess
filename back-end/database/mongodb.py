a630a150286c046d9ca05f56a124261ccd0cc24f
Le Quang Tung <lequangtung2002@gmail.com>
2024-06-06 16:05:56 +0700

    up: resset share db

--- back-end/database/mongodb.py
+++ back-end/database/mongodb.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 MONGO_URI = 'mongodb://localhost:27017/'
-MONGO_URI_ATLAS = 'mongodb+srv://admin:admin@db.sraixjh.mongodb.net/?retryWrites=true&w=majority&appName=DB'
+MONGO_URI_ATLAS = 'mongodb://admin:admin@ac-sbfny3p-shard-00-00.dqwy5sj.mongodb.net:27017,ac-sbfny3p-shard-00-01.dqwy5sj.mongodb.net:27017,ac-sbfny3p-shard-00-02.dqwy5sj.mongodb.net:27017/?ssl=true&replicaSet=atlas-uqyu3b-shard-0&authSource=admin&retryWrites=true&w=majority&appName=DB'
 MONGO_DB_NAME = 'chess'
 
 def get_mongo():
