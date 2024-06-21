718e7e06b7a72acae259cf5e56b25ecfdbe00ac2
Le Quang Tung <lequangtung2002@gmail.com>
2024-06-21 12:17:56 +0700

    fix: fix render chat gpt

--- front-end/src/components/chat/chatbox.jsx
+++ front-end/src/components/chat/chatbox.jsx
@@ -7,8 +7,7 @@ import {
   Spinner,
   Text,
 } from "@chakra-ui/react";
-import OpenAI from "openai";
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { IoSend } from "react-icons/io5";
 import { getUserData } from "../../lib/auth";
@@ -18,6 +17,7 @@ const ChatBox = () => {
   const user = getUserData();
   const theme = localStorage.getItem("theme");
   const { t } = useTranslation();
+
   const [openaiInstance, setOpenAIInstance] = useState(null);
   const [messages, setMessages] = useState([
     {
@@ -27,6 +27,7 @@ const ChatBox = () => {
     },
   ]);
   const [inputValue, setInputValue] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const suggestions = useMemo(
     () => [
@@ -38,69 +39,113 @@ const ChatBox = () => {
   );
 
   useEffect(() => {
-    const openai = new OpenAI({
-      apiKey: appSettings.OPENAI_KEY,
-      dangerouslyAllowBrowser: true,
-    });
-    setOpenAIInstance(openai);
+    const initializeOpenAI = async () => {
+      try {
+        const OpenAI = await import("openai");
+        const openai = new OpenAI.default({
+          apiKey: appSettings.OPENAI_KEY,
+          dangerouslyAllowBrowser: true,
+        });
+        setOpenAIInstance(openai);
+      } catch (error) {
+        console.error("Error initializing OpenAI:", error);
+      }
+    };
+
+    initializeOpenAI();
   }, []);
 
-  const sendMessage = useCallback(
-    async (text) => {
-      const trimmedInputValue = inputValue.trim();
-      const trimmedText = text ? text.trim() : "";
+  const sendMessage = async (messageContent) => {
+    const trimmedInputValue = messageContent.trim();
 
-      if (!trimmedInputValue && !trimmedText) return;
+    if (!trimmedInputValue) {
+      console.error("Message content is empty");
+      return;
+    }
 
-      const message = {
-        text: trimmedText || trimmedInputValue,
-        user_id: user?.id,
-        username: user?.username,
+    const userMessage = {
+      text: trimmedInputValue,
+      username: user?.username,
+      user_id: user?.id,
+    };
+
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      userMessage,
+      { text: "#loading", username: t("chat.assistant"), user_id: "assistant" },
+    ]);
+    setInputValue("");
+    setLoading(true);
+
+    const model = {
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        ...messages.map((item) => ({
+          role: item.user_id === "assistant" ? "assistant" : "user",
+          content: item.text,
+        })),
+        { role: "user", content: trimmedInputValue },
+      ],
+      model: "gpt-3.5-turbo",
+      max_tokens: 100,
+    };
+
+    try {
+      const response = await fetch(
+        "https://api.openai.com/v1/chat/completions",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${appSettings.OPENAI_KEY}`,
+          },
+          body: JSON.stringify(model),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log("Received response from OpenAI API:", data);
+
+      if (
+        !data ||
+        !data.choices ||
+        data.choices.length === 0 ||
+        !data.choices[0].message ||
+        !data.choices[0].message.content
+      ) {
+        console.error("Invalid response from API:", data);
+        return;
+      }
+
+      const botMessageContent = data.choices[0].message.content;
+      const botMessage = {
+        username: t("chat.assistant"),
+        text: botMessageContent,
+        user_id: "assistant",
       };
 
-      setMessages((prevMessages) => [
-        ...prevMessages,
-        message,
-        { username: "System", text: <Spinner size="sm" />, user_id: "system" },
-      ]);
-      setInputValue("");
-
-      try {
-        const completion = await openaiInstance.chat.completions.create({
-          messages: [
-            { role: "system", content: "You are a helpful assistant." },
-            ...messages.map((item) => ({
-              role: item.user_id === "assistant" ? "assistant" : "user",
-              content: item.text,
-            })),
-            { role: "user", content: trimmedText || trimmedInputValue },
-          ],
-          model: "gpt-3.5-turbo-0125",
-          max_tokens: 100,
-        });
-
-        const botMessageContent = completion.choices[0].message.content;
-        const botMessage = {
-          username: t("chat.assistant"),
-          text: botMessageContent,
-          user_id: "assistant",
-        };
-
-        setMessages((prevMessages) => [
-          ...prevMessages.slice(0, -1),
-          botMessage,
-        ]);
-      } catch (error) {
-        console.error("Error sending message:", error);
-      }
-    },
-    [inputValue, messages, openaiInstance, t, user]
-  );
+      setMessages((prevMessages) => {
+        const updatedMessages = [...prevMessages];
+        updatedMessages[updatedMessages.length - 1] = botMessage;
+        return updatedMessages;
+      });
+    } catch (error) {
+      console.error("Error sending message:", error);
+      // Handle specific errors or log them accordingly
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const handleInputChange = (e) => setInputValue(e.target.value);
+
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
-      sendMessage();
+      sendMessage(inputValue);
     }
   };
 
@@ -133,32 +178,36 @@ const ChatBox = () => {
             >
               @{message.username}
             </Text>
-            <Text
-              bgColor={
-                user?.id === message.user_id
-                  ? theme === "dark"
-                    ? "gray.600"
-                    : "black"
-                  : theme === "dark"
-                    ? "black"
-                    : "lightgray"
-              }
-              textAlign={user?.id === message.user_id ? "right" : "left"}
-              color={
-                user?.id === message.user_id
-                  ? theme === "dark"
-                    ? "black"
-                    : "white"
-                  : theme === "dark"
-                    ? "white"
-                    : "black"
-              }
-              px={4}
-              py={1}
-              borderRadius={8}
-            >
-              {message.text}
-            </Text>
+            {message.text === "#loading" ? (
+              <Spinner size="sm" />
+            ) : (
+              <Text
+                bgColor={
+                  user?.id === message.user_id
+                    ? theme === "dark"
+                      ? "gray.600"
+                      : "black"
+                    : theme === "dark"
+                      ? "black"
+                      : "lightgray"
+                }
+                textAlign={user?.id === message.user_id ? "right" : "left"}
+                color={
+                  user?.id === message.user_id
+                    ? theme === "dark"
+                      ? "black"
+                      : "white"
+                    : theme === "dark"
+                      ? "white"
+                      : "black"
+                }
+                px={4}
+                py={1}
+                borderRadius={8}
+              >
+                {message.text}
+              </Text>
+            )}
           </Box>
         ))}
         {messages?.length < 2 && user?.id && (
@@ -173,10 +222,10 @@ const ChatBox = () => {
           >
             {suggestions.map((item, idx) => (
               <Button
+                key={idx}
                 variant={"outline"}
                 colorScheme="gray"
                 borderRadius={20}
-                key={idx}
                 w={"100%"}
                 mb={2}
                 onClick={() => sendMessage(item)}
@@ -200,8 +249,9 @@ const ChatBox = () => {
           />
           <Button
             colorScheme="gray"
-            onClick={() => sendMessage()}
+            onClick={() => sendMessage(inputValue)}
             title={t("chat.send")}
+            disabled={loading}
           >
             <IoSend fontSize={24} />
           </Button>
--- front-end/src/pages/test/index.jsx
+++ front-end/src/pages/test/index.jsx
@@ -1,7 +1,24 @@
 import { AspectRatio, Box } from "@chakra-ui/react";
+import OpenAI from "openai";
 import React from "react";
+import appSettings from "../../settings/appSettings";
 
 export default function TestPage() {
+  async function main() {
+    const openai = new OpenAI({
+      apiKey: appSettings.OPENAI_KEY,
+      dangerouslyAllowBrowser: true,
+    });
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: "You are a helpful assistant." }],
+      model: "gpt-3.5-turbo",
+    });
+
+    console.log(completion.choices[0]);
+  }
+
+  main();
+
   return (
     <Box w={"48%"}>
       <AspectRatio w={"100%"} ratio={1}>
